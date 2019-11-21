d420e34c2b5eced8be569141295c7d77b152ec2d
chompa111 <gustavo.lach@gmail.com>
2019-11-20 22:47:18 -0300

    ajustes para rodar

--- main/principal.py
+++ main/principal.py
@@ -17,13 +17,13 @@ def extrair_politica_total(name,politica):
             f.write(estado+":"+politica[estado]+"\n")
 
 def extrair_politica_parcial(name,politica,problema):
-
+    count=0
     acoes=problema['action']
     with open (name + ".txt", "w") as f:
         s=problema['initialstate']
         meta=problema['goalstate']
         while s!=meta :
-
+            count+=1;
             f.write(s+":"+politica[s]+"\n")
 
             sucessores=acoes[politica[s]][s]
@@ -32,27 +32,28 @@ def extrair_politica_parcial(name,politica,problema):
             else:
                 sucessor=sucessores[0][0]
             s=sucessor
+    return count
 
 def extrair_resultados(nome,politica,problema):
-    extrair_politica_parcial(nome+"politica_parcial",politica,problema)
     extrair_politica_total(nome+"politica_total",politica)
+    return extrair_politica_parcial (nome + "politica_parcial", politica, problema)
 
 
 
 
 # print('Problemas determinísticos\n')
 gerar_graficos = False
-# for p in problemas_nomes:
-#     print('\nExecutando o problema {}'.format(p))
-#     t = time.time()
-#     lao=LAO_star(deterministic_instances[p], gerar_graficos)
-#     print('LAO * Executado em {} seg\n'.format(time.time() - t))
-#     # extrair_resultados(p+"_lao_",lao,deterministic_instances[p])
-#     t = time.time()
-#     iteracao=IteracaoDeValor.iteracaoDeValor().aplicar(problema=deterministic_instances[p], alpha=0.001, gerar_graficos=gerar_graficos)
-#     print('Iteracao de Valor Executado em {} seg'.format(time.time()-t))
-#     # extrair_resultados (p + "_iteracao_de_valor_", iteracao, deterministic_instances[p])
-#
+for p in problemas_nomes:
+    print('\nExecutando o problema {}'.format(p))
+    t = time.time()
+    lao=LAO_star(deterministic_instances[p], gerar_graficos)
+    print('LAO * Executado em {} seg'.format(time.time() - t))
+    print ("tamanho do plano {}\n".format(extrair_resultados(p+"_lao_",lao,deterministic_instances[p])))
+    t = time.time()
+    iteracao=IteracaoDeValor.iteracaoDeValor().aplicar(problema=deterministic_instances[p], alpha=0.001, gerar_graficos=gerar_graficos)
+    print('Iteracao de Valor Executado em {} seg'.format(time.time()-t))
+    print ("tamanho do plano {}\n".format(extrair_resultados (p + "_iteracao_de_valor_", iteracao, deterministic_instances[p])))
+
 
 print('\n\nProblemas aleatórios\n')
 for p in problemas_nomes:
@@ -60,13 +61,13 @@ for p in problemas_nomes:
     t = time.time ()
     lao = LAO_star (random_instances[p], gerar_graficos)
     print ('LAO * Executado em {} seg\n'.format (time.time () - t))
-    # extrair_resultados (p + "_lao", lao, deterministic_instances[p])
+    print ("tamanho do plano",extrair_resultados (p + "_lao", lao, deterministic_instances[p]))
     t = time.time ()
     iteracao = IteracaoDeValor.iteracaoDeValor ().aplicar (problema=random_instances[p], alpha=0.001,
                                                            gerar_graficos=gerar_graficos)
 
     print ('Iteracao de Valor Executado em {} seg'.format (time.time () - t))
-    # extrair_resultados (p + "_iteracao_de_valor", iteracao, deterministic_instances[p])
+    print("tamanho do plano"+extrair_resultados (p + "_iteracao_de_valor", iteracao, deterministic_instances[p]))
 
 
 
